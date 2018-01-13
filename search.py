90852e7f135a544900b03e6f528a79d160273591
wifijanitor <steve@wifijanitor.com>
2018-01-13 12:52:32 -0600

    PEP 8 updated

--- search.py
+++ search.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
-import  os,sys, re
-version=2.0
+import os
+import sys
+import re
+version = 2.0
 
-part=sys.argv[1]
-file=open(os.path.expanduser('~/Cisco/price.txt'))
+part = sys.argv[1]
+file = open(os.path.expanduser('~/Cisco/price.txt'))
 
-rgx=re.compile(part, re.I)
+rgx = re.compile(part, re.I)
 for line in file:
-	if re.search(rgx, line):
-		print(line)
+    if re.search(rgx, line):
+        print(line)
 file.close()
